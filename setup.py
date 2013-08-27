58c987067f0284fe1a2572247efbe7791ee31b48
Patrick Kimber <code@pkimber.net>
2013-08-27 17:49:10 +0100

    version 0.0.02

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def get_readme():
 
 setup(
     name='pkimber-cms',
-    packages=['cms', 'cms.tests', 'cms.management', 'cms.management.commands'],
+    packages=['cms', 'cms.migrations', 'cms.tests', 'cms.management', 'cms.management.commands'],
     package_data={
         'cms': [
             'static/*.*',
@@ -28,7 +28,7 @@ setup(
             'templates/cms/*.*',
         ],
     },
-    version='0.0.04',
+    version='0.0.02',
     description='cms',
     author='Patrick Kimber',
     author_email='code@pkimber.net',
