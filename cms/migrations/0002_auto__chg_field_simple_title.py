4e096c82d41ae507a01eed53fa939cf5eb23e14d
Patrick Kimber <code@pkimber.net>
2013-11-26 17:34:25 +0000

    convert 'title' to 'varchar'

---
+++ cms/migrations/0002_auto__chg_field_simple_title.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+
+        # Changing field 'Simple.title'
+        db.alter_column(u'cms_simple', 'title', self.gf('django.db.models.fields.TextField')())
+
+    def backwards(self, orm):
+
+        # Changing field 'Simple.title'
+        db.alter_column(u'cms_simple', 'title', self.gf('django.db.models.fields.CharField')(max_length=100))
+
+    models = {
+        u'cms.section': {
+            'Meta': {'ordering': "['name']", 'object_name': 'Section'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'cms.simple': {
+            'Meta': {'ordering': "['section', 'order', 'modified']", 'object_name': 'Simple'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'moderated': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'order': ('django.db.models.fields.IntegerField', [], {}),
+            'picture': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'blank': 'True'}),
+            'section': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['cms.Section']"}),
+            'title': ('django.db.models.fields.TextField', [], {}),
+            'url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['cms']
