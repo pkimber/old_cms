1dba045d4f0c99d501d2913afc09e0e31080c006
Patrick Kimber <code@pkimber.net>
2013-12-27 21:25:33 +0000

    simple forms

--- cms/forms.py
+++ cms/forms.py
@@ -1,1 +1,33 @@
 from django import forms
+
+from base.form_utils import (
+    RequiredFieldForm,
+)
+from .models import (
+    Section,
+)
+
+
+class SectionEmptyForm(forms.ModelForm):
+
+    class Meta:
+        model = Section
+        fields = ()
+
+
+class SectionForm(RequiredFieldForm):
+
+    def __init__(self, *args, **kwargs):
+        super(SectionForm, self).__init__(*args, **kwargs)
+        for name in ('title', 'description'):
+            self.fields[name].widget.attrs.update(
+                {'class': 'pure-input-2-3'}
+            )
+
+    class Meta:
+        model = Section
+        fields = (
+            'title',
+            'description',
+            'picture',
+        )
