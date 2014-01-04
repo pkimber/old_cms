b43ba3a2db2b53e41041bbc4591a3637c4538d8a
Patrick Kimber <code@pkimber.net>
2014-01-04 12:30:00 +0000

    template tag for adding content

--- cms/templatetags/cms_tags.py
+++ cms/templatetags/cms_tags.py
@@ -4,6 +4,14 @@ from django import template
 register = template.Library()
 
 
+@register.inclusion_tag('cms/_add.html')
+def cms_add(page_slug, layout_slug):
+    return dict(
+        page_slug=page_slug,
+        layout_slug=layout_slug,
+    )
+
+
 @register.inclusion_tag('cms/_moderate.html')
 def cms_moderate(content):
     return dict(content=content)
