c656a6765087dd8383ce13c75c9dd320e3798f8d
Patrick Kimber <code@pkimber.net>
2014-01-08 16:03:33 +0000

    init south

---
+++ cms/migrations/0001_initial.py
@@ -0,0 +1,126 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'ModerateState'
+        db.create_table(u'cms_moderatestate', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('slug', self.gf('django.db.models.fields.SlugField')(max_length=100)),
+        ))
+        db.send_create_signal(u'cms', ['ModerateState'])
+
+        # Adding model 'Page'
+        db.create_table(u'cms_page', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('created', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+            ('modified', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, blank=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('slug', self.gf('django.db.models.fields.SlugField')(unique=True, max_length=100)),
+            ('order', self.gf('django.db.models.fields.IntegerField')(default=0)),
+        ))
+        db.send_create_signal(u'cms', ['Page'])
+
+        # Adding model 'Layout'
+        db.create_table(u'cms_layout', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('created', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+            ('modified', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, blank=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('slug', self.gf('django.db.models.fields.SlugField')(unique=True, max_length=100)),
+        ))
+        db.send_create_signal(u'cms', ['Layout'])
+
+        # Adding model 'Section'
+        db.create_table(u'cms_section', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('created', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+            ('modified', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, blank=True)),
+            ('page', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['cms.Page'])),
+            ('layout', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['cms.Layout'])),
+        ))
+        db.send_create_signal(u'cms', ['Section'])
+
+        # Adding unique constraint on 'Section', fields ['page', 'layout']
+        db.create_unique(u'cms_section', ['page_id', 'layout_id'])
+
+        # Adding model 'Container'
+        db.create_table(u'cms_container', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('created', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+            ('modified', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, blank=True)),
+            ('section', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['cms.Section'])),
+            ('order', self.gf('django.db.models.fields.IntegerField')()),
+        ))
+        db.send_create_signal(u'cms', ['Container'])
+
+
+    def backwards(self, orm):
+        # Removing unique constraint on 'Section', fields ['page', 'layout']
+        db.delete_unique(u'cms_section', ['page_id', 'layout_id'])
+
+        # Deleting model 'ModerateState'
+        db.delete_table(u'cms_moderatestate')
+
+        # Deleting model 'Page'
+        db.delete_table(u'cms_page')
+
+        # Deleting model 'Layout'
+        db.delete_table(u'cms_layout')
+
+        # Deleting model 'Section'
+        db.delete_table(u'cms_section')
+
+        # Deleting model 'Container'
+        db.delete_table(u'cms_container')
+
+
+    models = {
+        u'cms.container': {
+            'Meta': {'object_name': 'Container'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'order': ('django.db.models.fields.IntegerField', [], {}),
+            'section': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['cms.Section']"})
+        },
+        u'cms.layout': {
+            'Meta': {'ordering': "('name',)", 'object_name': 'Layout'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '100'})
+        },
+        u'cms.moderatestate': {
+            'Meta': {'ordering': "['name']", 'object_name': 'ModerateState'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '100'})
+        },
+        u'cms.page': {
+            'Meta': {'ordering': "['name']", 'object_name': 'Page'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'order': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '100'})
+        },
+        u'cms.section': {
+            'Meta': {'ordering': "['page', 'modified']", 'unique_together': "(('page', 'layout'),)", 'object_name': 'Section'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'layout': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['cms.Layout']"}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'page': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['cms.Page']"})
+        }
+    }
+
+    complete_apps = ['cms']
