a022e47d94e207dad1cf64a058f12f6b47345729
Patrick Kimber <code@pkimber.net>
2014-01-09 13:25:11 +0100

    version 0.0.14

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
             'templates/cms/*.*',
         ],
     },
-    version='0.0.13',
+    version='0.0.14',
     description='cms',
     author='Patrick Kimber',
     author_email='code@pkimber.net',
