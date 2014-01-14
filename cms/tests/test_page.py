90998d393e62a9d4996d20d4b0dca2cf26cbe2df
Patrick Kimber <code@pkimber.net>
2014-01-14 22:22:32 +0000

    add 'is_home' to page model

--- cms/tests/test_page.py
+++ cms/tests/test_page.py
@@ -6,29 +6,17 @@ from cms.tests.model_maker import make_page
 
 class TestContent(TestCase):
 
-    def _menu_list_slugs(self):
-        return Page.objects.menu().values_list('slug', flat=True)
-
     def setUp(self):
         self.page = make_page('Home', 0)
         self.page = make_page('Information', 1)
         self.page = make_page('Portfolio', 2)
 
     def test_menu(self):
-        self.assertEqual(2, len(Page.objects.menu()))
-
-    def test_menu_not_home(self):
-        self.assertNotIn(
-            'home',
-            self._menu_list_slugs()
-        )
+        self.assertEqual(3, len(Page.objects.menu()))
 
     def test_menu_in(self):
-        self.assertIn(
-            'information',
-            self._menu_list_slugs()
-        )
-        self.assertIn(
-            'portfolio',
-            self._menu_list_slugs()
+        result = [p.slug for p in Page.objects.menu()]
+        self.assertListEqual(
+            ['home', 'information', 'portfolio'],
+            result
         )
