9887818e2607cdced769737d3eec1b8a3dfc6911
Patrick Kimber <code@pkimber.net>
2014-01-28 13:01:54 +0000

    model no longer user

--- cms/models.py
+++ cms/models.py
@@ -54,50 +54,6 @@ class ModerateState(models.Model):
 reversion.register(ModerateState)
 
 
-class ModerateModel(models.Model):
-    """
-    An abstract base class model that allows simple moderation.
-    """
-    moderate_state = models.ForeignKey(
-        ModerateState,
-        default=default_moderate_state
-    )
-    date_moderated = models.DateTimeField(blank=True, null=True)
-    user_moderated = models.ForeignKey(
-        settings.AUTH_USER_MODEL, blank=True, null=True, related_name='+'
-    )
-
-    class Meta:
-        abstract = True
-        unique_together = ('section', 'moderate_state')
-
-    def _is_pending(self):
-        return self.moderate_state == ModerateState.pending()
-    is_pending = property(_is_pending)
-
-    def _is_published(self):
-        return self.moderate_state == ModerateState.published()
-    is_published = property(_is_published)
-
-    def _is_removed(self):
-        return self.moderate_state == ModerateState.removed()
-    is_removed = property(_is_removed)
-
-    def _set_moderated(self, user, moderate_state):
-        self.date_moderated = datetime.now()
-        self.user_moderated = user
-        self.moderate_state = moderate_state
-
-    def _set_pending(self, user):
-        self._set_moderated(user, moderatestate.pending())
-
-    def _set_published(self, user):
-        self._set_moderated(user, ModerateState.published())
-
-    def _set_removed(self, user):
-        self._set_moderated(user, ModerateState.removed())
-
-
 class PageManager(models.Manager):
 
     def menu(self):
